ee5cc339af99440ee2ee990dd1e69da757664532
SILVAIN Nicolas <nicolas.silvain@outlook.com>
2019-03-30 21:37:49 +0100

    Deploy to Heroku

--- src/main/java/org/distribution/cupofglory/config/HerokuDatabaseConfiguration.java
+++ src/main/java/org/distribution/cupofglory/config/HerokuDatabaseConfiguration.java
@@ -4,7 +4,8 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
@@ -20,21 +21,27 @@ public class HerokuDatabaseConfiguration implements EnvironmentAware {
 
     private final Logger log = LoggerFactory.getLogger(HerokuDatabaseConfiguration.class);
 
-    private RelaxedPropertyResolver propertyResolver;
+//    private RelaxedPropertyResolver propertyResolver;
 
     private Environment environment;
+
+    @Value("spring.datasource.heroku-url")
+    private String herokuUrl;
+
+    @Value("spring.datasource.dataSourceClassName")
+    private String dataSourceClassName;
 
     @Override
     public void setEnvironment(Environment environment) {
         this.environment = environment;
-        this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
+//        this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
     }
 
     @Bean
     public DataSource dataSource() {
         log.debug("Configuring Heroku Datasource");
 
-        String herokuUrl = propertyResolver.getProperty("heroku-url");
+//        String herokuUrl = propertyResolver.getProperty("heroku-url");
         if (herokuUrl != null) {
             log.info("Using Heroku, parsing their $DATABASE_URL to use it with JDBC");
             URI dbUri = null;
@@ -53,7 +60,8 @@ public class HerokuDatabaseConfiguration implements EnvironmentAware {
                 "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
             HikariConfig config = new HikariConfig();
-            config.setDataSourceClassName(propertyResolver.getProperty("dataSourceClassName"));
+//            config.setDataSourceClassName(propertyResolver.getProperty("dataSourceClassName"));
+            config.setDataSourceClassName(dataSourceClassName);
             config.addDataSourceProperty("url", dbUrl);
             config.addDataSourceProperty("user", username);
             config.addDataSourceProperty("password", password);
